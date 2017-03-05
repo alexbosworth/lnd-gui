2781034a261ec902263de14897776306f04acb03
Alex Bosworth <alex.bosworth@gmail.com>
2017-03-05 14:18:23 -0800

    Initial Commit

---
+++ lnd-gui/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lnd-gui
+//
+//  Created by Alex Bosworth on 3/5/17.
+//  Copyright © 2017 Adylitica. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
