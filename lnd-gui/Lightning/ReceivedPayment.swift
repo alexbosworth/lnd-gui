da43b25003a9db193c38b97e5ac4667a0576368a
Alex Bosworth <alex.bosworth@gmail.com>
2017-04-02 17:35:06 -0700

    Abstract, merge interface code changes

---
+++ lnd-gui/Lightning/ReceivedPayment.swift
@@ -0,0 +1,19 @@
+//
+//  ReceivedPayment.swift
+//  lnd-gui
+//
+//  Created by Alex Bosworth on 4/2/17.
+//  Copyright © 2017 Adylitica. All rights reserved.
+//
+
+import Foundation
+
+/** ReceivedPayment represents an invoiced payment.
+ */
+struct ReceivedPayment {
+  let amount: UInt64
+  let confirmed: Bool
+  let createdAt: Date
+  let memo: String
+  let payment: String
+}
--- lnd-gui/ViewControllers/MainViewController.swift
+++ lnd-gui/ViewControllers/MainViewController.swift
@@ -13,34 +13,68 @@ import Cocoa
  FIXME: - cleanup, comment
  */
 class MainViewController: NSViewController {
+  // MARK: - @IBOutlets
+  
+  /** balanceLabelTextField is the balance label that shows the amount of funds available.
+   */
+  @IBOutlet weak var balanceLabelTextField: NSTextField?
+
+  /** connectedBox is the box that reflects the last known connected state to the LN daemon.
+   */
+  @IBOutlet weak var connectedBox: NSBox?
+  
   // MARK: - Properties
   
-  private var chainBalance: Value? {
-    didSet {
-      updateVisibleBalance()
-    }
-  }
-  
-  /** channelBalance is the value of the total funds in channels.
-   
-   FIXME: - use localizable string
+  /** chainBalance represents the amount of available value on chain.
    */
-  private var channelBalance: Value? {
-    didSet {
-      updateVisibleBalance()
-    }
-  }
+  fileprivate var chainBalance: Value? { didSet { updateVisibleBalance() } }
   
-  private var connected: Bool? {
-    didSet {
-      updateConnectedStatus()
-    }
-  }
+  /** channelBalance represents the value of the total funds in channels.
+   */
+  fileprivate var channelBalance: Value? { didSet { updateVisibleBalance() } }
+
+  /** connected represents whether or not there a connection is present to the backing ln daemon.
+   */
+  fileprivate var connected: Bool? { didSet { updateConnectedStatus() } }
   
   /** mainTabViewController is the tab view controller for the main view
    */
   weak var mainTabViewController: MainTabViewController?
 
+  /** receivedPayments represents payments received.
+   
+    FIXME: - abstract out notification
+   */
+  var receivedPayments: [ReceivedPayment]? {
+    willSet {
+      guard let pastPayments = receivedPayments, let newPayments = newValue else { return }
+      
+      var payments = [String: ReceivedPayment]()
+      
+      pastPayments.forEach { payments[$0.payment] = $0 }
+      
+      let confirmationChanged = newPayments.filter { payment in
+        guard let previousInvoice = payments[payment.payment] else { return false }
+        
+        return previousInvoice.confirmed != payment.confirmed
+      }
+      
+      guard !confirmationChanged.isEmpty else { return }
+      
+      refreshBalances() {}
+      
+      confirmationChanged.forEach { payment in
+        let notification = NSUserNotification()
+        
+        notification.title = "Payment for \(payment.memo)"
+        notification.informativeText = "Received \(payment.amount)"
+        notification.soundName = NSUserNotificationDefaultSoundName
+        
+        NSUserNotificationCenter.default.deliver(notification)
+      }
+    }
+  }
+  
   // MARK: - UIViewController
   
   /** initWalletPolling kicks off wallet polling
@@ -57,20 +91,16 @@ class MainViewController: NSViewController {
     )
   }
   
-  /** viewDidLoad method initializes the view controller.
+  /** updateConnectedStatus updates the view to reflect the current connected state.
+   
+   FIXME: - use nicer colors
    */
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    refreshChannelBalance {}
-    
-    initWalletPolling()
-    
-    connected = false
-    
-    balanceLabelTextField?.stringValue = String()
+  func updateConnectedStatus() {
+    connectedBox?.fillColor = (connected ?? false) as Bool ? .green : .red
   }
-  
+
+  /** updateVisibleBalance updates the view to show the last retrieved balances
+   */
   private func updateVisibleBalance() {
     let chainBalance = (self.chainBalance ?? Value()) as Value
     let channelBalance = (self.channelBalance ?? Value()) as Value
@@ -81,20 +111,93 @@ class MainViewController: NSViewController {
     balanceLabelTextField?.stringValue = formattedBalance
   }
 
-  @IBOutlet weak var balanceLabelTextField: NSTextField?
+  /** viewDidLoad method initializes the view controller.
+   */
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    refreshBalances {}
+    
+    initWalletPolling()
+    
+    connected = false
+    
+    balanceLabelTextField?.stringValue = String()
+  }
+}
+
+// MARK: - Errors
+extension MainViewController {
+  enum Failure: String, Error {
+    case expectedMainTabViewController
+  }
+}
+
+// MARK: - Navigation
+extension MainViewController {
+  /** prepare performs setup for the navigated-to view controller.
+   */
+  override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+    super.prepare(for: segue, sender: sender)
+    
+    guard let mainTabViewController = segue.destinationController as? MainTabViewController else {
+      return print(Failure.expectedMainTabViewController.localizedDescription)
+    }
+    
+    mainTabViewController.updateBalance = { [weak self] in self?.refreshBalances() {} }
+    
+    self.mainTabViewController = mainTabViewController
+  }
+}
+
+// MARK: - Networking
+extension MainViewController {
   
-  @IBOutlet weak var connectedBox: NSBox?
-  
-  struct ReceivedPayment {
-    let amount: UInt64
-    let confirmed: Bool
-    let createdAt: Date
-    let memo: String
-    let payment: String
+  /** refreshBalances updates the chain and channel balance from the LN daemon.
+   
+   FIXME: - switch to sockets
+   */
+  func refreshBalances(completion: (() -> ())?) {
+    let url = URL(string: "http://localhost:10553/v0/balance/")!
+    let session = URLSession.shared
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    request.cachePolicy = .reloadIgnoringCacheData
+    
+    let task = session.dataTask(with: request) { [weak self] data, urlResponse, error in
+      guard let balanceData = data else { return print("Expected balance data") }
+      
+      let dataDownloadedAsJson = try? JSONSerialization.jsonObject(with: balanceData, options: .allowFragments)
+      
+      let balance = dataDownloadedAsJson as? [String: Any]
+      
+      enum BalanceResponseJsonKey: String {
+        case chainBalance = "chain_balance"
+        case channelBalance = "channel_balance"
+        
+        var key: String { return rawValue }
+      }
+      
+      let chainBalance = (balance?[BalanceResponseJsonKey.chainBalance.key] as? NSNumber)?.uint64Value
+      let channelBalance = (balance?[BalanceResponseJsonKey.channelBalance.key] as? NSNumber)?.uint64Value
+      
+      DispatchQueue.main.async {
+        self?.chainBalance = chainBalance
+        self?.channelBalance = channelBalance
+      }
+      
+      completion?()
+    }
+    
+    task.resume()
   }
   
-  // FIXME: - switch to sockets
-  public func refreshInvoices() {
+  /** refreshInvoices performs a data request to the LN daemon to fetch information about outstanding invoices.
+   
+   // FIXME: - switch to sockets
+   */
+  func refreshInvoices() {
     let url = URL(string: "http://localhost:10553/v0/invoices/")!
     let session = URLSession.shared
     
@@ -140,102 +243,5 @@ class MainViewController: NSViewController {
     }
     
     task.resume()
-  }
-  
-  var receivedPayments: [ReceivedPayment]? {
-    willSet {
-      guard let pastPayments = receivedPayments, let newPayments = newValue else { return }
-
-      var payments = [String: ReceivedPayment]()
-      
-      pastPayments.forEach { payments[$0.payment] = $0 }
-      
-      let confirmationChanged = newPayments.filter { payment in
-        guard let previousInvoice = payments[payment.payment] else { return false }
-        
-        return previousInvoice.confirmed != payment.confirmed
-      }
-      
-      guard !confirmationChanged.isEmpty else { return }
-      
-      refreshChannelBalance() {}
-      
-      confirmationChanged.forEach { payment in
-        let notification = NSUserNotification()
-        
-        notification.title = "Payment for \(payment.memo)"
-        notification.informativeText = "Received \(payment.amount)"
-        notification.soundName = NSUserNotificationDefaultSoundName
-        
-        NSUserNotificationCenter.default.deliver(notification)
-      }
-    }
-  }
-  
-  func updateConnectedStatus() {
-    let connected = (self.connected ?? false) as Bool
-    
-    connectedBox?.fillColor = connected ? .green : .red
-  }
-  
-  // FIXME: - switch to sockets
-  func refreshChannelBalance(completion: (() -> ())?) {
-    let url = URL(string: "http://localhost:10553/v0/balance/")!
-    let session = URLSession.shared
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = "GET"
-    request.cachePolicy = .reloadIgnoringCacheData
-    
-    let task = session.dataTask(with: request) { [weak self] data, urlResponse, error in
-      guard let balanceData = data else { return print("Expected balance data") }
-      
-      let dataDownloadedAsJson = try? JSONSerialization.jsonObject(with: balanceData, options: .allowFragments)
-      
-      let balance = dataDownloadedAsJson as? [String: Any]
-
-      enum BalanceResponseJsonKey: String {
-        case chainBalance = "chain_balance"
-        case channelBalance = "channel_balance"
-        
-        var key: String { return rawValue }
-      }
-
-      let chainBalance = (balance?[BalanceResponseJsonKey.chainBalance.key] as? NSNumber)?.uint64Value
-      let channelBalance = (balance?[BalanceResponseJsonKey.channelBalance.key] as? NSNumber)?.uint64Value
-      
-      DispatchQueue.main.async {
-        self?.chainBalance = chainBalance
-        self?.channelBalance = channelBalance
-      }
-      
-      completion?()
-    }
-    
-    task.resume()
-  }
-}
-
-// MARK: - Errors
-extension MainViewController {
-  enum Failure: String, Error {
-    case expectedMainTabViewController
-  }
-}
-
-// MARK: - Navigation
-extension MainViewController {
-  /** prepare performs setup for the navigated-to view controller.
-   */
-  override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
-    super.prepare(for: segue, sender: sender)
-    
-    guard let mainTabViewController = segue.destinationController as? MainTabViewController else {
-      return print(Failure.expectedMainTabViewController.localizedDescription)
-    }
-    
-    mainTabViewController.updateBalance = { [weak self] in self?.refreshChannelBalance() {} }
-    
-    self.mainTabViewController = mainTabViewController
   }
 }
